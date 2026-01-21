4f2efc5a5474297e4aaf9f220781403af5932cc6
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-01-21 17:06:42 +0200

    up

--- app/Http/Middleware/SetLocaleMiddleware.php
+++ app/Http/Middleware/SetLocaleMiddleware.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 class SetLocaleMiddleware
 {
     /**
-     * Handle an incoming request.
+     * Handle an incoming request. 
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
